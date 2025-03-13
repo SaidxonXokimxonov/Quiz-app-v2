4c487188f799921abbc364e4ae0891cfb1db4a6a
SaidxonXokimxonov <saidxonxokimxonov006@gmail.com>
2025-03-13 12:25:33 +0500

    Proyect ishlash uchun tayyor

--- src/elements.ts
+++
@@ -1,3 +0,0 @@
-export const wrapper: HTMLDivElement = document.querySelector('#wrapper')!
-export const indLvl: HTMLSpanElement = document.querySelector('#indlvl')!
-export const indScore: HTMLSpanElement  = document.querySelector('#score')!
